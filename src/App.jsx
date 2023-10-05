df572be992e9046c4fe81506f0d9d2917611b585
Anthony Bosek <anthony.bosek@gmail.com>
2023-10-04 20:14:14 -0700

    add useEffect w/ fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { X_AUTH_TOKEN } from "../config";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [params, setParams] = useState({ selector: "persons", id: 8004 });
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    console.log(X_AUTH_TOKEN);
+    const { selector, id } = params;
+    fetch(`https://api.football-data.org/v4/${selector}/${id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth-Token": X_AUTH_TOKEN,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
+      {console.log(data)}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +47,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
